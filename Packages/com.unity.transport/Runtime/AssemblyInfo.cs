02630456c93229749b8914b4b55b41c7f8f3add4
jacob.lorentzen@unity3d.com <>
2021-09-02 10:00:02 -0700

    Moved to Public transport package.

--- Packages/com.unity.transport/Runtime/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.Networking.Transport.EditorTests")]
-[assembly: InternalsVisibleTo("Unity.Multiplayer.Transport.UTP")]
-
