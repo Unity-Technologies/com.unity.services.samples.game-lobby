f82134fd2ee23eac2934b948212b24f457eaf422
nathaniel.buck@unity3d.com <>
2021-09-13 18:37:44 -0700

    DTLS integration, probably? It's not in this Editor version, but this should work in 2021. Added a test to say "hey, this is the wrong version" and everything still works the same as before adding DTLS.

---
+++ Assets/Scripts/Tests/PlayMode/UtpTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LobbyRelaySample.relay;
+using NUnit.Framework;
+using Unity.Networking.Transport;
+using Unity.Services.Relay.Models;
+using UnityEngine.TestTools;
+
+namespace Test
+{
+    public class UtpTests
+    {
+        private class RelayUtpTest : RelayUtpSetupHost
+        {
+            public NetworkEndPoint GetEndpointButPublic(List<RelayServerEndpoint> endpoints, string ip, int port, out bool isSecure)
+            {
+                return GetEndpointForAllocation(endpoints, ip, port, out isSecure);
+            }
+        }
+
+        [UnityTest]
+        public IEnumerator DTLSCheck()
+        {
+            yield return null; // TEMP
+
+            #if ENABLE_MANAGED_UNITYTLS
+                Assert.Fail("TODO: Implement.");
+            #else
+                Assert.Ignore("DTLS is not available in this version of Unity.");
+            #endif
+        }
+    }
+}
