aa7bb65c09ebf5e69eacefb61a4a9c8c2758104a
nathaniel.buck@unity3d.com <>
2021-10-29 10:30:46 -0700

    Missed a comment

--- Assets/Scripts/Game/InGame/SymbolObject.cs
+++ Assets/Scripts/Game/InGame/SymbolObject.cs
@@ -41,7 +41,7 @@ namespace LobbyRelaySample.inGame
         public void Destroy_ServerRpc()
         {
             // Actually destroying the symbol objects can cause garbage collection and other delays that might lead to desyncs.
-            // Instead, just deactivate the object, and it will be cleaned up once the NetworkManager is destroyed.
+            // Disabling the networked object can also cause issues, so instead, just move the object, and it will be cleaned up once the NetworkManager is destroyed.
             // (If object pooling, this is where to instead return it to the pool.)
             this.transform.localPosition = Vector3.down * 500;
             // TODO: Visually disappear immediately.
