79d4a00f59414fe69af2f719f7b0dd372d8000db
jacob.lorentzen@unity3d.com <>
2021-12-07 10:59:51 -0800

    Whitespace fix

--- Assets/Scripts/Lobby/LobbyAsyncRequests.cs
+++ Assets/Scripts/Lobby/LobbyAsyncRequests.cs
@@ -7,7 +7,7 @@ using Unity.Services.Lobbies.Models;
 namespace LobbyRelaySample
 {
     /// <summary>
-    /// An abstraction layer between the direct calls into the Lobby API and the outcomes you actually want. E.g. you can request to get a readable list of 
+    /// An abstraction layer between the direct calls into the Lobby API and the outcomes you actually want. E.g. you can request to get a readable list of
     /// current lobbies and not need to make the query call directly.
     /// </summary>
     public class LobbyAsyncRequests
@@ -299,7 +299,7 @@ namespace LobbyRelaySample
             Lobby lobby = m_lastKnownLobby;
             Dictionary<string, DataObject> dataCurr = lobby.Data ?? new Dictionary<string, DataObject>();
 
-			var shouldLock = false;
+            var shouldLock = false;
             foreach (var dataNew in data)
             {
                 // Special case: We want to be able to filter on our color data, so we need to supply an arbitrary index to retrieve later. Uses N# for numerics, instead of S# for strings.
@@ -309,14 +309,14 @@ namespace LobbyRelaySample
                     dataCurr[dataNew.Key] = dataObj;
                 else
                     dataCurr.Add(dataNew.Key, dataObj);
-                
+
                 //Special Use: Get the state of the Local lobby so we can lock it from appearing in queries if it's not in the "Lobby" State
                 if (dataNew.Key == "State")
                 {
                     Enum.TryParse(dataNew.Value, out LobbyState lobbyState);
                     shouldLock = lobbyState != LobbyState.Lobby;
                 }
-            }         
+            }
 
             LobbyAPIInterface.UpdateLobbyAsync(lobby.Id, dataCurr, shouldLock, (result) =>
             {
@@ -337,7 +337,6 @@ namespace LobbyRelaySample
                 m_rateLimitQuery.EnqueuePendingOperation(caller);
                 return false;
             }
-
             Lobby lobby = m_lastKnownLobby;
             if (lobby == null)
             {
